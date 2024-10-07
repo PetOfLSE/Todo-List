78acfd4e4313f49d0589166884f52dd3177cf77b
unname <lwer210@naver.com>
2024-10-08 00:52:21 +0900

    swagger 어노테이션 추가

--- todo-backend/src/main/java/com/example/todo/user/controller/UserController.java
+++ todo-backend/src/main/java/com/example/todo/user/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.example.todo.user.controller;
 
+import com.example.todo.user.controller.request.RegisterRequest;
+import com.example.todo.user.model.entity.UserEntity;
+import com.example.todo.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(){
+    private final UserService userService;
 
+    @Operation(summary = "회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공시 200")
+    })
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest) {
+        UserEntity register = userService.register(registerRequest);
+        return ResponseEntity.ok(register);
     }
 }
