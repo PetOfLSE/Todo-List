c54dc804bb8aedc4d6ae6e51cf18c11c3866d97d
unname <lwer210@naver.com>
2024-10-07 23:52:58 +0900

    Todo 테이벌 연동

---
+++ todo-backend/src/main/java/com/example/todo/todo/model/entity/TodoEntity.java
@@ -0,0 +1,45 @@
+package com.example.todo.todo.model.entity;
+
+import com.example.todo.user.model.entity.UserEntity;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "todo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @Column(nullable = false, name = "body")
+    private String body;
+
+    @Column(nullable = false, name = "complete_at")
+    private LocalDateTime completeAt;
+
+    @Column(nullable = false, name = "create_at")
+    private LocalDateTime createAt;
+
+    @Column(nullable = false, name = "complete")
+    private Boolean complete;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
