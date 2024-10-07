e3a69b49b1b1e868f6ac0f164ce28da13d5314dd
unname <lwer210@naver.com>
2024-10-07 23:53:50 +0900

    User Entity

---
+++ todo-backend/src/main/java/com/example/todo/user/model/entity/UserEntity.java
@@ -0,0 +1,44 @@
+package com.example.todo.user.model.entity;
+
+import com.example.todo.todo.model.entity.TodoEntity;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "user_id", unique = true)
+    private String userId;
+
+    @Column(nullable = false, name = "password", unique = true)
+    private String password;
+
+    @Column(nullable = false, name = "email", unique = true)
+    private String email;
+
+    private String role;
+
+    private String gender;
+
+    private LocalDateTime registerAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<TodoEntity> todos;
+}
