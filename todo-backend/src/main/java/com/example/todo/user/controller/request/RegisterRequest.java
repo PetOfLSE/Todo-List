d3bb0a3b50e1ca13fa86a947b704e6f5649d2689
unname <lwer210@naver.com>
2024-10-08 00:52:00 +0900

    name 필드 추가

--- todo-backend/src/main/java/com/example/todo/user/controller/request/RegisterRequest.java
+++ todo-backend/src/main/java/com/example/todo/user/controller/request/RegisterRequest.java
@@ -2,7 +2,9 @@ package com.example.todo.user.controller.request;
 
 import com.example.todo.common.enums.Gender;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -13,14 +15,20 @@ import lombok.*;
 public class RegisterRequest {
 
     @NotNull
+    @Max(value = 50)
     private String userId;
 
     @NotNull
+    @Size(min = 4, max = 50)
     private String password;
+
+    @NotNull
+    private String name;
 
     @Email
     private String email;
 
     @NotNull
     private Gender gender;
+
 }
