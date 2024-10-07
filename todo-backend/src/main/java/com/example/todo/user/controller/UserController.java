77d71d02e39e7a2b7e5fecbca8be7765a3388a16
unname <lwer210@naver.com>
2024-10-07 23:58:12 +0900

    User API 컨트롤러

---
+++ todo-backend/src/main/java/com/example/todo/user/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.todo.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(){
+
+    }
+}
