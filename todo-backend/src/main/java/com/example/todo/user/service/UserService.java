da26de9c0d55aa3a268a8684dc030ab9f71326a1
unname <lwer210@naver.com>
2024-10-10 00:25:20 +0900

    예외 처리 추가

--- todo-backend/src/main/java/com/example/todo/user/service/UserService.java
+++ todo-backend/src/main/java/com/example/todo/user/service/UserService.java
@@ -8,6 +8,7 @@ import com.example.todo.user.controller.request.RegisterRequest;
 import com.example.todo.user.model.entity.UserEntity;
 import com.example.todo.user.model.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
@@ -30,14 +32,21 @@ public class UserService {
     }
 
     public JwtResponseDto login(LoginRequest loginRequest) {
-        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword()));
+        try{
+            Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword()));
 
-        String accessToken = jwtUtil.generateToken(authenticate);
-        String refreshToken = jwtUtil.generateRefreshToken(authenticate);
+            String accessToken = jwtUtil.generateToken(authenticate);
+            String refreshToken = jwtUtil.generateRefreshToken(authenticate);
 
-        return JwtResponseDto.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
+            return JwtResponseDto.builder()
+                    .accessToken(accessToken)
+                    .refreshToken(refreshToken)
+                    .build();
+        }catch (Exception e){
+            log.info("UserService login Fail : {}", e.getMessage());
+        }
+
+        return null;
+
     }
 }
