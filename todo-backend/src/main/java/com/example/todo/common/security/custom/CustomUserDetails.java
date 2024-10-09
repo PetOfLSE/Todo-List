da6ecf022076404867dd7750bc066be2e456572d
unname <lwer210@naver.com>
2024-10-09 22:30:19 +0900

    UserDetails 인터페이스 구현

--- todo-backend/src/main/java/com/example/todo/common/security/custom/CustomUserDetails.java
+++ todo-backend/src/main/java/com/example/todo/common/security/custom/CustomUserDetails.java
@@ -6,26 +6,31 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
-
-    private final UserEntity user;
+    private final UserEntity userEntity;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+        List<SimpleGrantedAuthority> role = Arrays.stream(new String[]{userEntity.getRole().name()})
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        return role;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return userEntity.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return user.getUserId();
+        return userEntity.getUserId();
     }
 }
