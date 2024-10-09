bbdb01ab671db169b987dbb353af06c5593f9a6e
unname <lwer210@naver.com>
2024-10-09 22:19:43 +0900

    Security Config 수정

--- todo-backend/src/main/java/com/example/todo/common/security/config/SecurityConfig.java
+++ todo-backend/src/main/java/com/example/todo/common/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.todo.common.security.config;
 
 import com.example.todo.common.security.custom.CustomUserDetailsService;
+import com.example.todo.common.security.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -27,8 +30,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http
                 .authorizeRequests(auth -> {
                     auth.requestMatchers("/**").permitAll()
+                            .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
                             .requestMatchers("/api/user/**").permitAll()
                             .anyRequest().authenticated();
                 });
