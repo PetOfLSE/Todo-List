2a3c286d05619b4acb5f7482eb30031a39782194
unname <lwer210@naver.com>
2024-10-10 00:57:17 +0900

    토큰 재발급 비즈니스 로직

--- todo-backend/src/main/java/com/example/todo/user/service/UserService.java
+++ todo-backend/src/main/java/com/example/todo/user/service/UserService.java
@@ -1,18 +1,23 @@
 package com.example.todo.user.service;
 
 import com.example.todo.common.converter.Converter;
+import com.example.todo.common.security.jwt.JwtRefreshDto;
 import com.example.todo.common.security.jwt.JwtResponseDto;
 import com.example.todo.common.security.jwt.JwtUtil;
 import com.example.todo.user.controller.request.LoginRequest;
 import com.example.todo.user.controller.request.RegisterRequest;
 import com.example.todo.user.model.entity.UserEntity;
 import com.example.todo.user.model.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +53,37 @@ public class UserService {
 
         return null;
 
+    }
+
+    public JwtRefreshDto refresh(HttpServletRequest request) {
+        String token = request.getHeader("Refresh-Token");
+        log.info("token : {}", token);
+
+        try{
+            if (token != null) {
+                Claims claims = jwtUtil.parseToken(token);
+                log.info("claim : {}", claims.toString());
+
+                Date exp = new Date(((Integer) claims.get("exp")).longValue() * 1000);
+                if (exp.after(new Date())) {
+
+                    Long id = Long.valueOf((Integer) claims.get("id"));
+                    UserEntity user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User Not Found"));
+
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUserId(), user.getPassword());
+                    String accessToken = jwtUtil.generateToken(authentication);
+
+                    return JwtRefreshDto.builder()
+                            .accessToken(accessToken)
+                            .build();
+                } else {
+                    throw new RuntimeException("Refresh Token 만료");
+                }
+            }
+        }catch (Exception e){
+            log.info("UserService refresh Fail : {}", e.getMessage());
+        }
+
+        throw new RuntimeException("UserService refresh Fail");
     }
 }
