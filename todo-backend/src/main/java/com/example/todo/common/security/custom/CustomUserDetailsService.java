70614399e2135b27d8c4e520e67a6db0a2d25899
unname <lwer210@naver.com>
2024-10-09 23:53:32 +0900

    find 메서드 확인

--- todo-backend/src/main/java/com/example/todo/common/security/custom/CustomUserDetailsService.java
+++ todo-backend/src/main/java/com/example/todo/common/security/custom/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.example.todo.common.security.custom;
 import com.example.todo.user.model.entity.UserEntity;
 import com.example.todo.user.model.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,14 +11,18 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity byName = userRepository.findByName(username);
+        log.info("username : {}", username);
+        UserEntity byName = userRepository.findByUserId(username);
+        log.info("user : {}", byName);
         if(byName == null) {
+            log.info("User Not Found : CustomUserDetailsService");
             throw new UsernameNotFoundException(username);
         }
         return new CustomUserDetails(byName);
