cff2779f92ce677ec79eec03be97fc6ece81666a
unname <lwer210@naver.com>
2024-10-10 23:30:32 +0900

    할일 추가 Response

--- todo-backend/src/main/java/com/example/todo/todo/controller/response/TodoAddResponse.java
+++ todo-backend/src/main/java/com/example/todo/todo/controller/response/TodoAddResponse.java
@@ -1,10 +1,6 @@
 package com.example.todo.todo.controller.response;
 
 import com.example.todo.todo.model.entity.TodoEntity;
-import com.example.todo.user.model.entity.UserEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -27,15 +23,12 @@ public class TodoAddResponse {
 
     private Boolean complete;
 
-    private UserEntity user;
-
     public static TodoAddResponse toTodoResponse(TodoEntity entity){
         return TodoAddResponse.builder()
                 .id(entity.getId())
                 .content(entity.getContent())
                 .complete(entity.getComplete())
                 .completeAt(entity.getCompleteAt())
-                .user(entity.getUser())
                 .createAt(entity.getCreateAt())
                 .build();
     }
