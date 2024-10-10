7e5acdad78de69cd7f14edcef95392b2feb09b13
unname <lwer210@naver.com>
2024-10-10 22:34:57 +0900

    todo 비즈니스 로직

---
+++ todo-backend/src/main/java/com/example/todo/todo/service/TodoService.java
@@ -0,0 +1,28 @@
+package com.example.todo.todo.service;
+
+import com.example.todo.todo.controller.request.TodoAddRequest;
+import com.example.todo.todo.controller.response.TodoAddResponse;
+import com.example.todo.todo.model.entity.TodoEntity;
+import com.example.todo.todo.model.repository.TodoRepository;
+import com.example.todo.user.model.entity.UserEntity;
+import com.example.todo.user.model.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+
+    private final UserRepository userRepository;
+    private final TodoRepository todoRepository;
+
+    public TodoAddResponse add(Long id, TodoAddRequest request) {
+        UserEntity user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+
+        TodoEntity todoEntity = TodoAddRequest.toTodoEntity(request, user);
+        TodoEntity save = todoRepository.save(todoEntity);
+
+        TodoAddResponse response = TodoAddResponse.toTodoResponse(save);
+        return response;
+    }
+}
