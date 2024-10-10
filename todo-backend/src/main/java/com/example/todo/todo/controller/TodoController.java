b0659c4d2a694a8c4439f87706bd5b5d50a65281
unname <lwer210@naver.com>
2024-10-11 02:41:44 +0900

    Todo 삭제 API 추가

--- todo-backend/src/main/java/com/example/todo/todo/controller/TodoController.java
+++ todo-backend/src/main/java/com/example/todo/todo/controller/TodoController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -59,5 +60,18 @@ public class TodoController {
         TodoResponse patch = todoService.patch(id, request, modifyRequest);
 
         return ResponseEntity.ok(patch);
+    }
+
+    @Operation(summary = "할일 삭제")
+    @Parameter(name = "id", description = "Todo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공시 200 반환"),
+            @ApiResponse(responseCode = "400", description = "실패시 400 반환")
+    })
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id, HttpServletRequest request){
+        boolean delete = todoService.delete(id, request);
+
+        return (delete) ? ResponseEntity.ok("삭제가 완료되었습니다.") : ResponseEntity.status(HttpStatus.BAD_REQUEST).body("삭제에 실패하였습니다.");
     }
 }
